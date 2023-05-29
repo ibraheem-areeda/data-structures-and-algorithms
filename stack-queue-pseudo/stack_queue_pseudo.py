f7cbd96572d008b2cad8ebfccfaf93cd7dc58f1e
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-05-29 19:34:27 +0000

    first commit

---
+++ stack-queue-pseudo/stack_queue_pseudo.py
@@ -0,0 +1,92 @@
+class Node:
+    def __init__(self, value, next_=None):
+        """
+        Initialize a new Node object.
+
+        Args:
+            value: The value to be stored in the node.
+            next_: Reference to the next node in the linked list. Default is None.
+        """
+        self.value = value
+        self.next_ = next_
+
+    def __str__(self):
+        """
+        Return a string representation of the Node.
+
+        Returns:
+            A string representation of the value stored in the Node.
+        """
+        return f"{self.value}"
+
+
+class Stack:
+    def __init__(self, top=None):
+        """
+        Initialize a new Stack object.
+
+        Args:
+            top: The top node of the stack. Default is None.
+        """
+        self.top = top
+
+    def __str__(self):
+        """
+        Return a string representation of the Stack.
+
+        Returns:
+            A string representation of the value stored in the top node of the Stack.
+            If the stack is empty, returns "This stack is empty".
+        """
+        if self.top is None:
+            return "this stack is empty"
+        return f"the stack top value = {self.top.value}"
+
+    def push(self, value):
+        """
+        Pushes a value onto the top of the stack.
+
+        Args:
+            value: The value to be pushed onto the stack.
+        """
+        self.top = Node(value, self.top)
+
+    def pop(self):
+        """
+        Removes and returns the value from the top of the stack.
+
+        Returns:
+            The value that was removed from the top of the stack.
+            If the stack is empty, returns "This stack is empty".
+        """
+        if self.top is None:
+            raise Exception("this stack is empty")
+        temp = self.top
+        self.top = temp.next_
+        temp.next_ = None
+        return temp.value
+
+    def peek(self):
+        """
+        Returns the value from the top of the stack without removing it.
+
+        Returns:
+            The value from the top of the stack.
+            If the stack is empty, raise AttributeError and print "This stack is empty".
+        """
+        return self.top.value
+
+
+class  PseudoQueue ():
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+    
+    def enqueue(self,value):
+        self.stack1.push(value)
+
+    def dequeue(self):
+        pass
+    
+
+
