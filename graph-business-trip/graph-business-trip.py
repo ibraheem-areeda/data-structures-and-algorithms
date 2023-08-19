fd430c906151aebaafd6b3247b4a1bb7117b7a3c
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-08-19 21:52:43 +0000

    debuging

--- graph-business-trip/graph-business-trip.py
+++ graph-business-trip/graph-business-trip.py
@@ -1,12 +1,162 @@
-# Challenge Title
-<!-- Description of the challenge -->
+from collections import deque 
 
-## Whiteboard Process
-<!-- Embedded whiteboard image -->
+class Queue:
 
-## Approach & Efficiency
-<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+    def __init__(self):
+        self.dq = deque()
 
-## Solution
-<!-- Show how to run your code, and examples of it in action -->
+    def enqueue(self, value):
+        self.dq.append(value)
 
+    def dequeue(self):
+        return self.dq.popleft()
+
+    def __len__(self):
+        return len(self.dq)
+
+
+
+class Vertix:
+
+    def __init__(self, value):
+
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class Edge:
+
+    def __init__(self, vertix, weight=0):
+        self.weight = weight
+        self.vertix = vertix
+
+    def __str__(self):
+        return str(self.vertix) + str(self.weight)
+
+class Graph:
+
+    def __init__(self):
+        self.__adj_list = {}
+      
+    def add_vertix(self,value):
+      '''
+      Arguments: value
+      Returns: The added vertex
+      Add a vertex to the graph
+      '''
+  
+      vertix = Vertix(value)
+      self.__adj_list[vertix] = []
+      return vertix
+
+  
+
+    def add_edge(self, start_vertix, end_vertix, weight=0):
+        '''
+        Arguments: 2 vertices to be connected by the edge, weight (optional)
+        Returns: nothing
+        Adds a new edge between two vertices in the graph
+        If specified, assign a weight to the edge
+        Both vertices should already be in the Graph
+        '''
+        if start_vertix not in self.__adj_list:
+            raise KeyError("Start vertex is not found")
+        if end_vertix not in self.__adj_list:
+            raise KeyError("End vertex is not found")
+        edge1 = Edge(end_vertix, weight)
+        edge2 = Edge(start_vertix)
+        self.__adj_list[start_vertix].append(edge1)
+        self.__adj_list[end_vertix].append(edge2)
+
+  
+  
+    def get_vertices(self):
+      '''
+        Arguments: none
+        Returns all of the vertices in the graph as a  
+        collection (set, list, or similar)
+        Empty collection returned if there are no vertices
+      '''
+  
+      return self.__adj_list.keys()
+
+  
+    def size(self):
+      return len(self.__adj_list)
+  
+    def get_neighbors(self,vertix):
+      '''
+      get neighbors
+      A rguments: vertex
+      Returns a collection of edges connected to the 
+      given vertex
+      Include the weight of the connection in the returned collection
+      Empty collection returned if there are no vertices
+      '''
+      
+      return self.__adj_list.get(vertix, [])
+  
+    def breadth_first(self,start_vertix):
+    
+        result = []
+        visted = set()
+        q = Queue()
+
+        q.enqueue(start_vertix)
+        visted.add(start_vertix)
+
+        while len(q):
+            current_vertix = q.dequeue()
+
+            result.append(current_vertix.value)
+
+            neighbors =self.get_neighbors(current_vertix)
+
+            for edge in neighbors:
+                neighbor = edge.vertix
+                if neighbor not in visted:
+                    q.enqueue(neighbor)
+                    visted.add(neighbor)
+
+        return result
+        
+    
+def business_trip(graph, cities):
+    total_cost = 0
+    
+    for i in range(len(cities) - 1):
+        current_city = cities[i]
+        next_city = cities[i + 1]
+        print(current_city)
+        print(next_city)
+        path = []
+        for city in graph.get_neighbors(current_city):
+            path.append(city.vertix.value)
+            print(path)
+        if str(next_city) not in path :
+            print(1111111,next_city)
+            return None
+
+    
+    return total_cost
+    
+
+if __name__ == "__main__":
+    g = Graph()
+    a = g.add_vertix('A')
+    b = g.add_vertix('B')
+    e = g.add_vertix('E')
+    c = g.add_vertix('C')
+    d = g.add_vertix('D')
+
+    g.add_edge(a,b,5)
+    g.add_edge(a,c,5)
+    g.add_edge(b,d,5)
+    g.add_edge(b,e)
+    g.add_edge(e,d)
+    g.add_edge(e,c)
+    print(g.breadth_first(a))
+    arr = [a,b,c,d]
+    print(business_trip(g,arr))
